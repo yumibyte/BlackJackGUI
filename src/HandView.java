41fd2c97dc3b2244912f3cf1a327e395cb32814b
Yumibyte <ashraigosa@gmail.com>
2020-11-20 14:23:09 -0800

    able to set posiiton within class and instantiate multiple hands

--- src/HandView.java
+++ src/HandView.java
@@ -1,9 +1,15 @@
 import javax.swing.*;
 
 public class HandView {
-    JPanel panel = new JPanel();
-    JLabel cardImage = new JLabel("hi");
 
-
-
+    int positionX;
+    int positiony;
+    String imageName;
+    JLabel imageLabel;
+    HandView(int positionX, int positiony, String imageName) {
+        this.positionX = positionX;
+        this.positiony = positiony;
+        imageLabel = new JLabel(imageName);
+        imageLabel.setBounds(positionX, positiony, 50, 100);
+    }
 }
