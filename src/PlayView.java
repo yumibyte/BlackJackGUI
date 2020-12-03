43277a32f6fda0f3c78f52366e572b69dd321266
Yumibyte <ashraigosa@gmail.com>
2020-12-02 22:40:54 -0800

    change positioning

--- src/PlayView.java
+++ src/PlayView.java
@@ -15,15 +15,15 @@ public class PlayView {
         ImageIcon blackJackBackground = new ImageIcon("BlackJackImage.jpg");
         background.setIcon(blackJackBackground);
         background.setBounds(200, 400, 1000, 600);
-        background.setLayout(new FlowLayout(FlowLayout.CENTER));
+//        background.setLayout(new FlowLayout(FlowLayout.CENTER));
         playViewPanel.add(background);
 
         hitButton = new JButton("Hit!");
-        hitButton.setBounds(50, 50, 100, 50);
+        hitButton.setBounds(100, 50, 175, 50);
         background.add(hitButton);
 
         standButton = new JButton("Stand");
-        standButton.setBounds(300, 50, 100, 50);
+        standButton.setBounds(700, 50, 175, 50);
         background.add(standButton);
 
     }
